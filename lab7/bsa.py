6ee16019b76a3ce9132e5b9b73720afed2deef04
Ganesh P Nischay <ganeshnischayp@gmail.com>
2019-03-13 09:48:27 +0530

    block tbd

--- lab7/bsa.py
+++ lab7/bsa.py
@@ -21,9 +21,9 @@ def factors_func(n):
         i=i+1
     return factors
 
-m = int(input("Enter M: "))
+m = (input("Enter M: "))
 e = int(input("Enter e: "))
-n = int(input("Enter N "))
+n = int(input("Enter N: "))
 r = int(input("Enter r: "))
 
 factors = factors_func(n)
@@ -34,13 +34,66 @@ print("private key = ",d)
 r_inverse = modInverse(r,n)
 print("r inverse wrt mod(N)",r_inverse)
 
-# fucker attacker
-sends_to_sign = (m * (r ** e)) % n
 
-# noob bob
-sends_signed = (sends_to_sign ** d) % n
+x=8
+while(n>(2**x)):
+    if(n>(2**(x+8))):
+        x=x+8
+    else:
+        break
+block_size = x/8
 
-# fucker attacker receives
-attacker_recieved = (sends_signed * r_inverse) % n
+block_size = int(block_size)
+print(int(block_size))
+num_blocks =len(m) /  (block_size)
+num_blocks = int(num_blocks)
+arr = []
 
-print("Blind signature attack: ",attacker_recieved)
+
+for i in range((num_blocks)):
+    arr.append(m[i:i+block_size])
+    
+arr = ['NI','TK','57','50','25']
+
+
+
+print(arr)
+
+for i in range(len(arr)):
+    hexa_caoncat = ''
+    for j in arr[i]:
+        ascii = ord(j)
+        hexa_caoncat += (hex(ascii).replace('0x',''))
+    # print(hexa_caoncat)
+    in_dec = int(hexa_caoncat,16)
+
+
+    sends_to_sign = (in_dec * (r ** e)) % n
+    sends_signed = (sends_to_sign ** d) % n
+    attacker_recieved = (sends_signed * r_inverse) % n
+    # print("Blind signature attack: ",attacker_recieved)
+    attacker_recieved_in_readiable = chr(attacker_recieved)
+    print("Block ",arr[i]," Signature : ",attacker_recieved_in_readiable)
+
+
+
+
+
+
+
+
+
+
+# # fucker attacker
+# sends_to_sign = (m * (r ** e)) % n
+
+# # noob bob
+# sends_signed = (sends_to_sign ** d) % n
+
+# # fucker attacker receives
+# attacker_recieved = (sends_signed * r_inverse) % n
+
+# print("Blind signature attack: ",attacker_recieved)
+
+
+
