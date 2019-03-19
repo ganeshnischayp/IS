ebe0edc2cefbf51e9f6d41242de80e5510799e1b
Ganesh P Nischay <ganeshnischayp@gmail.com>
2019-03-19 23:32:03 +0530

    lab 7 over

--- lab8/chosen.py
+++ lab8/chosen.py
@@ -4,7 +4,7 @@ def modInverse(a, m) :
     for x in range(1, m) : 
         if ((a * x) % m == 1) : 
             return x 
-    return 1
+    return -1
 
 def factors_func(n):
     factors = []
@@ -21,21 +21,48 @@ def factors_func(n):
                 factors.append(i)
         i=i+1
     return factors
-p = int(input("enter p: "))
-q = int(input("enter q: "))
+# p = int(input("enter p: "))
+# q = int(input("enter q: "))
 m = (input("Enter M: "))
 
-n = p * q
+n = int(input("Enter N: "))
 e = int(input("Enter e: "))
+
+
 factors = factors_func(n)
 # print("factors of N = ",*factors)
-# phi_of_n = (factors[0]-1)*(factors[1]-1)
-phi_of_n = (p-1)*(q-1)
+phi_of_n = (factors[0]-1)*(factors[1]-1)
+# phi_of_n = (p-1)*(q-1)
 d = modInverse(e,phi_of_n)
-print(d)
+
+if d == -1:
+    print("d dosent exist: ")
+    exit()
+
+print("Private key ",d)
+
 r = int(input("Enter r: "))
-r_inverse = modInverse(r,n)
-print(r_inverse)
+count = 0
+flag = 0
+while count < 3:
+    count += 1
+    r_inverse = modInverse(r,n)
+    if r_inverse != -1:
+        flag = 1
+        break
+    else:
+        if count == 3:
+            break
+        r = int(input("Enter r: "))
+
+if flag == 0:
+    print("r inverse dosent exist")
+    exit()
+
+
+
+print("r_inverse ",r_inverse)
+
 block_size = (math.floor(math.log(n)/math.log(2))+1) // 8
 
 block_size = int(block_size)
@@ -53,7 +80,7 @@ for k in range(len(arr)):
         hexa_caoncat += (hex(ascii).replace('0x',''))
     # print(hexa_caoncat)
     in_dec = int(hexa_caoncat,16)
-
+    # print(in_dec)//////////////////////////////
 
     sends_to_sign = (in_dec * (r ** e)) % n
     sends_signed = (sends_to_sign ** d) % n
@@ -62,7 +89,16 @@ for k in range(len(arr)):
     attacker_recieved_in_hex = hex(attacker_recieved).replace('0x','')
     # print(attacker_recieved_in_hex)
 
-    final_hex_arr = [attacker_recieved_in_hex[i:i+2] for i in range(0,len(attacker_recieved_in_hex),block_size)]
+    if len(attacker_recieved_in_hex)%2 != 0:
+        attacker_recieved_in_hex = '0' + attacker_recieved_in_hex
+
+
+    final_hex_arr = [attacker_recieved_in_hex[i:i+2] for i in range(0,len(attacker_recieved_in_hex),2)]
+        
+
+
+
+
     # print(final_hex_arr)
     #  = int(attacker_recieved_in_hex,16)
 
@@ -74,9 +110,11 @@ for k in range(len(arr)):
 
         # print(in_int,str(in_chr))
         final_ans += chr(int(final_hex_arr[i],16))
-
-    print("Block ",arr[k]," Signature : ",attacker_recieved, "Hexadecimal = ",attacker_recieved_in_hex," Split Hexa = ",final_hex_arr, "Human  = ",final_ans)
-
+    
+    checking = (attacker_recieved ** e) % n 
+    # print(" Checking ",checking,)
+    # print("Block ",arr[k], " Attacker Received : ",attacker_recieved, "Hexadecimal = ",attacker_recieved_in_hex," Split Hexa = ",final_hex_arr, "Human  = ",final_ans)
+    print("Block ",arr[k]," Attacker Received :  ",attacker_recieved,"Hexadecimal = ",attacker_recieved_in_hex," Split Hexa = ",final_hex_arr)
 
 
 
